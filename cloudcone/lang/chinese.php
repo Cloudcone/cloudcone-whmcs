63d32437ca8dc8c44f1b3fbcb1002c20fbb7fdb5
Ray Asxc <61820811+Ray-Asxc@users.noreply.github.com>
2021-01-17 19:45:06 +0800

    Create chinese.php

---
+++ cloudcone/lang/chinese.php
@@ -0,0 +1,54 @@
+<?php
+
+if ( ! defined( "WHMCS" ) ) {
+    die( "This file cannot be accessed directly" );
+}
+
+// Version 1.0.0
+$_LANG['ccone']['cloudcone'] = 'CloudCone';
+$_LANG['ccone']['cpu'] = 'CPU';
+$_LANG['ccone']['generate_config_notice'] = '保存之前点击!';
+$_LANG['ccone']['cpu_description'] = 'CPU内核的数量';
+$_LANG['ccone']['ram'] = 'RAM';
+$_LANG['ccone']['ram_description'] = 'RAM以MB为单位';
+$_LANG['ccone']['disk'] = '磁盘';
+$_LANG['ccone']['disk_description'] = '磁盘空间(GB)';
+$_LANG['ccone']['plan'] = '计划';
+$_LANG['ccone']['plan_description'] = '默认情况下为空，私有管理程序为227';
+$_LANG['ccone']['hypervisor'] = '虚拟机监控程序';
+$_LANG['ccone']['hypervisor_description'] = '选择虚拟机监控程序';
+$_LANG['ccone']['generate_options'] = '生成';
+$_LANG['ccone']['table_create_error'] = '错误!无法创建%s表';
+$_LANG['ccone']['clientarea_details_title'] = '细节';
+$_LANG['ccone']['clientarea_actions_title'] = '行动';
+$_LANG['ccone']['clientarea_access_title'] = '访问';
+$_LANG['ccone']['clientarea_action_boot'] = '开机';
+$_LANG['ccone']['clientarea_action_reboot'] = '重启';
+$_LANG['ccone']['clientarea_action_shutdown'] = '关机';
+$_LANG['ccone']['clientarea_action_rebuild'] = '重新安装';
+$_LANG['ccone']['clientarea_action_resetroot'] = 'Root密码重置';
+$_LANG['ccone']['status'] = '状态';
+$_LANG['ccone']['clientarea_password'] = 'Root密码';
+$_LANG['ccone']['clientarea_password_toggle'] = '显示/隐藏';
+$_LANG['ccone']['clientarea_os'] = '操作系统';
+$_LANG['ccone']['clientarea_cpu'] = 'CPU';
+$_LANG['ccone']['clientarea_cpu_description'] = 'vCPU';
+$_LANG['ccone']['clientarea_ram'] = '专用内存';
+$_LANG['ccone']['clientarea_ram_description'] = 'GB';
+$_LANG['ccone']['clientarea_disk'] = '磁盘大小';
+$_LANG['ccone']['clientarea_disk_description'] = 'GB';
+$_LANG['ccone']['clientarea_bandwidth'] = '带宽使用情况';
+$_LANG['ccone']['clientarea_bandwidth_description'] = '%1$s GB of %2$s TB Used';
+$_LANG['ccone']['clientarea_viewgraphs'] = '使用图表';
+$_LANG['ccone']['clientarea_viewconsole'] = '控制台';
+$_LANG['ccone']['clientarea_viewrebuild'] = '重新安装操作系统';
+$_LANG['ccone']['clientarea_viewresetroot'] = 'Root密码重置';
+$_LANG['ccone']['clientarea_graphs_title'] = '使用图表';
+$_LANG['ccone']['clientarea_console_title'] = '控制台';
+$_LANG['ccone']['clientarea_rebuild_title'] = '重新安装操作系统';
+$_LANG['ccone']['clientarea_rebuild_notice'] = '按“重新安装”按钮后，重新安装约需2至5分钟。';
+$_LANG['ccone']['clientarea_rebuild_alert'] = '警告!重新安装将格式化您的服务器磁盘。服务器上的所有当前数据都将丢失。';
+$_LANG['ccone']['clientarea_resetroot_title'] = 'Root密码重置';
+$_LANG['ccone']['clientarea_resetroot_notice'] = '重置Root密码大约需要2分钟，在您点击重置按钮后完成。';
+$_LANG['ccone']['clientarea_resetroot_text'] = '重新设置Root密码后，可以在信息页面上找到新密码。';
+$_LANG['ccone']['unauthorized'] = '拒绝访问';
